90f3ff62f0b2f5bf32451f22b42c6b1ca210e708
parom <E.Parominskiy@isida.by>
2024-03-04 09:37:38 +0300

    14. Real Time Example With JSON Message

---
+++ src/main/java/home/parom/RealTimeExample.java
@@ -0,0 +1,39 @@
+package home.parom;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.json.JSONObject;
+
+import javax.jms.*;
+
+/**
+ * @author E.Parominsky 04/03/2024 09:21
+ */
+public class RealTimeExample {
+
+    public static void main(String[] args) {
+        ConnectionFactory factory = new ActiveMQConnectionFactory("system", "system", "tcp://localhost:61616");
+        Connection connection = null;
+        try {
+            connection = factory.createConnection();
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Destination destination = session.createQueue("mytestqueue");
+
+            JSONObject json = new JSONObject();
+            json.put("from_date", "01-Jan-2019");
+            json.put("to_date", "31-Dec-2019");
+            json.put("email", "xyz@gmail.com");
+            json.put("query", "select * from data");
+
+            TextMessage textMessage = session.createTextMessage(json.toString());
+            MessageProducer producer = session.createProducer(destination);
+            producer.send(textMessage);
+
+            session.close();
+            connection.close();
+
+        } catch (JMSException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
