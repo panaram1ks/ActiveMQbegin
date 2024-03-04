65c22111497644667857c03fb874336d84b2383e
parom <E.Parominskiy@isida.by>
2024-03-04 09:08:41 +0300

    11. What is Client Acknoledgment ?

--- src/main/java/home/parom/Consumer.java
+++ src/main/java/home/parom/Consumer.java
@@ -15,7 +15,8 @@ public class Consumer {
             Connection connection = factory.createConnection();
             connection.start();
 
-            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+//            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE); // notify queue that the message is consumed
+            Session session = connection.createSession(false, Session.CLIENT_ACKNOWLEDGE); // do not notify queue
             Destination destination = session.createQueue("mytestqueue");
 
             MessageConsumer consumer = session.createConsumer(destination);
@@ -25,6 +26,7 @@ public class Consumer {
                     TextMessage textMessage = (TextMessage) message;
                     try {
                         System.out.println(textMessage.getText());
+                        textMessage.acknowledge(); // manual send acknowledge
                     } catch (JMSException e) {
                         throw new RuntimeException(e);
                     }
